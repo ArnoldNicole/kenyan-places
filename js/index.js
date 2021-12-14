94261f5c49430502fb2b40a896e6c90c90055b70
arnoldwamae <arnoldwamae2@gmail.com>
2021-12-14 17:45:41 +0300

    Html

--- js/index.js
+++ js/index.js
@@ -4,25 +4,37 @@ const subcounty = document.getElementById('subcounty');
 const ward = document.getElementById('ward');
 
 let url = 'https://raosys.com/js/kenyan_county_sub_county_ward.json';
-const xhr = new XMLHttpRequest();
 var locationsData = {
     counties: [],
     sub_counties: [],
     wards: []
 }
 
-// /var data = {};
+
 getData(url);
 
-// Select County
+// Select Sub- County
 select.addEventListener('change', function (e) {
+    ward.style.display = 'none';
+    subcounty.style.display = 'none';
     let i = locationsData.counties.findIndex(county => county.id == e.target.value)
     if (i != -1) {
         while (subcounty.firstChild) {
             subcounty.removeChild(subcounty.firstChild);
         }
         addOptions(locationsData.counties[i], subcounty)
+    }
+})
 
+// Select WARD
+subcounty.addEventListener('change', function (e) {
+    let i = locationsData.sub_counties.findIndex(sub_county => sub_county.id == e.target.value)
+    // console.log(i);
+    if (i != -1) {
+        while (ward.firstChild) {
+            ward.removeChild(ward.firstChild);
+        }
+        addWardOptions(locationsData.sub_counties[i], ward)
     }
 })
 
@@ -32,15 +44,11 @@ async function getData(url) {
     locationsData.counties = data.counties;
     locationsData.sub_counties = data.sub_counties;
     locationsData.wards = data.wards;
-
-
     locationsData.counties.forEach(county => {
         var opt = document.createElement('option');
         opt.text = `${county.name}`;
         opt.value = `${county.id}`
         select.appendChild(opt);
-        // let opt = `< option value = "${county.id}" > ${ county.name }</option > `;
-        // select.innerHTML = (opt);
     });
 }
 
@@ -57,19 +65,14 @@ function addOptions(county, select) {
 
 }
 
-
-
-
-
-// fetch(url)
-//     .then((response) => {
-//         return response.json()
-//     })
-//     .then((data) => {
-//         // Work with JSON data here
-//         counties = data.counties
-
-//     })
-//     .catch((err) => {
-//         // Do something for an error here
-//     })
+function addWardOptions(subcounty, ward_div) {
+    locationsData.wards.forEach(sel_ward => {
+        if (sel_ward.sub_county == subcounty.id) {
+            let opt = document.createElement('option');
+            opt.text = `${sel_ward.name}`;
+            opt.value = `${sel_ward.id}`
+            ward_div.appendChild(opt);
+            ward_div.style.display = ""
+        }
+    });
+}
